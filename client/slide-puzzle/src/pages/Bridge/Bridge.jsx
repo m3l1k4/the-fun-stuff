1c950bb903ef0c736c673370606fbc47088976b9
kauvero <kavehrt60@gmail.com>
2020-07-02 22:37:23 -0700

    link to main page added to bridge

--- client/slide-puzzle/src/pages/Bridge/Bridge.jsx
+++ client/slide-puzzle/src/pages/Bridge/Bridge.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import Header from "../../components/Header";
 import "./Bridge.scss";
+
 
 
 
@@ -9,8 +11,9 @@ export default function Bridge() {
         <>
         <Header/>
         <div className="bridge">
+        <Link className="link" to="/"><h3>back to Main</h3></Link>
             <div className="bridge__text-wrapper">
-                <h1 className="bridge__title">Not so fast </h1>
+                <h1 className="bridge__title">Not so fast !!!</h1>
                 <p className="bridge__hint">Many ways to cry , one way to puzzle</p>
             </div>
         </div>
