7f741972f1ad6f16f2bc7e761670feb334470026
m3l1k4 <melikaihelas@gmail.com>
2020-07-02 18:36:28 -0700

    Update slide.js

---
+++ puzzle/slide.js
@@ -0,0 +1,8 @@
+//slice image
+//drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+
+// randomize position of tiles
+// save original loaction of tiles
+
+// allow click for the image to move
+// each click moves the tile in a single direction
