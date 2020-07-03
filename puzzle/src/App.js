c60d65578e16363c0ca19a729409679dec69ffca
m3l1k4 <melikaihelas@gmail.com>
2020-07-03 10:09:09 -0700

    adding API bug : 2 renders

--- puzzle/src/App.js
+++ puzzle/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import "./App.scss";
 import Main from "./pages/Main";
 import Game from "./pages/Game";
@@ -8,15 +9,34 @@ import {Route,Switch, BrowserRouter as Router} from "react-router-dom";
 
 
 class App extends React.Component {
+
+// state = {
+//   puzzlePic:[]
+// }
+
+
+// componentDidMount(){
+// axios.get(" https://api.thecatapi.com/v1/images/search")
+// .then(res=>{
+//   this.setState({puzzlePic:res.data[0].url})
+//    console.log(res.data[0].url, "photoURL from app.js")
+   
+// })
+// }
+
+
   render() {
     return( 
-      <Router>
+      
         <Switch>
           <Route path="/" exact component={Main}/> 
-          <Route path="/onewaytopuzzle" component={Game}/>
+          <Route path="/manywaystocry" component={Game}  >
+          
+          </Route>
+
           <Route path="/manywaystocry" component={Bridge}/>
         </Switch> 
-      </Router>
+ 
       
     )
   }
--- puzzle/src/pages/Game/Game.jsx
+++ puzzle/src/pages/Game/Game.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./Game.scss";
 import {Link} from "react-router-dom";
-import puzzle from "../../components/Hero/puzzle-image.jpg";
 
 
-export default function Game() {
+
+export default function Game(puzzleImg) {
+
+    console.log(puzzleImg)
+    if (puzzleImg === undefined || puzzleImg.length == 0){
+        console.log("zero aray")
+    }
     return (
 
         <>
@@ -15,7 +20,7 @@ export default function Game() {
         </div>
         <div className="main">
             <Link className="link" to="/"><h3>back to Main</h3></Link>
-            <img className="main__puzzle" src={puzzle}></img>  
+            <img className="main__puzzle" src={puzzleImg}></img>  
         </div>
         </>
 
